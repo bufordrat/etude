6bda1e8f8219bd2b02a5d744aef9bcca09564399
Matt Teichman <teichman@uchicago.edu>
2023-11-10 21:53:38 -0600

    first draft of new config module

--- src/concrete/config.ml
+++ src/concrete/config.ml
@@ -1,39 +1,6 @@
-(* helper code for reading configuration information out of the
-   filesystem/shell environment *)
+let read_paths filepaths =
+  let open Option in
+  asum (List.map (catch Prelude.readfile) filepaths)
 
-module Env = struct
-  type (_,_) opt_or_def =
-    | ReturnOption : ('a, 'a option) opt_or_def
-    | DefaultTo : 'a -> ('a, 'a) opt_or_def
-
-  let _lookup
-          : type a. (string, a) opt_or_def -> string -> a =
-    fun what_to_do str ->
-    let open Prelude.Unix.Env in
-    let open Stdlib.List in
-    let value = env () |> assoc_opt str in
-    match value with
-    | Some v -> begin
-        match what_to_do with
-        | ReturnOption -> Some v
-        | DefaultTo _ -> v
-      end
-    | None -> begin
-        match what_to_do with
-        | ReturnOption -> None
-        | DefaultTo d -> d
-      end
-
-  (* let lookup  *)
-
-  (* module type MONAD = Endofunctors_intf.MONAD *)
-  (* module R = Endofunctors_old.Result.Make (String) *)
-
-
-(* let getenv = Prelude.Result.trapc
- *                "not in environment"
- *                Sys.getenv
- * let lookup env_vars =
- *   kleisli (List.map getenv env_vars) *)
-  (* end *)
-end
+let paths_fallback fallback filepaths =
+  Option.default fallback (read_paths filepaths)
