994070c61d6807fc76c33f9b99dbf5953dac58b1
Matt Teichman <teichman@uchicago.edu>
2023-11-11 16:10:45 -0600

    TODO: add version of alternatives with a parser

--- src/concrete/config.ml
+++ src/concrete/config.ml
@@ -1,29 +1,38 @@
-module Standard = struct
-  let alternatives f lst =
-    let open Option in
-    asum (List.map (catch f) lst)
+module File = struct
+  module Simple = struct
+    let alternatives f lst =
+      let open Option in
+      asum (List.map (catch f) lst)
 
-  let read_paths paths = alternatives Prelude.readfile paths
+    let read_paths paths = alternatives Prelude.readfile paths
 
-  let read_vars vars = alternatives Unix.getenv vars
+    let read_vars vars = alternatives Unix.getenv vars
 
-  let get_config ~path_vars ~paths ~fallback =
-    let open Option in
-    read_vars path_vars <|> read_paths paths <|> pure fallback
-    |> get
-end
-include Standard
+    let get_config ~path_vars ~paths ~fallback =
+      let open Option in
+      read_vars path_vars <|> read_paths paths <|> pure fallback
+      |> get
 
-module FCM = struct
-  module type CONFPATHS = sig
-    val path_vars : string list
-    val paths : string list
-    val fallback : string
+    (* let get_config ~path_vars ~paths ~fallback =
+     *   let open Option in
+     *   read_vars path_vars <|> read_paths paths <|> pure fallback
+     *   |> get *)
   end
 
-  let get_config (module ConfPaths : CONFPATHS) =
-    Standard.get_config
-      ~path_vars:ConfPaths.path_vars
-      ~paths:ConfPaths.paths
-      ~fallback:ConfPaths.fallback
+  let alternatives = Simple.alternatives
+  let get_config = Simple.get_config
+
+  module FCM = struct
+    module type CONFPATHS = sig
+      val path_vars : string list
+      val paths : string list
+      val fallback : string
+    end
+
+    let get_config (module ConfPaths : CONFPATHS) =
+      Simple.get_config
+        ~path_vars:ConfPaths.path_vars
+        ~paths:ConfPaths.paths
+        ~fallback:ConfPaths.fallback
+  end
 end
