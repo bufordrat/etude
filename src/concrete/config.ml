15fa9a9536ca3b0499b4c8383290a655559abd5b
Matt Teichman <teichman@uchicago.edu>
2023-11-11 22:29:50 -0600

    fleshing out a little more of the config code

--- src/concrete/config.ml
+++ src/concrete/config.ml
@@ -1,35 +1,35 @@
-module File = struct
+let alternatives lookup lst =
+  let open Option in
+  asum (List.map (catch lookup) lst)
+
+let alternatives_parse parse lookup lst =
+  let open Option in
+  asum (List.map (catch lookup) lst) >>= parse
+
+module Full = struct
   module Simple = struct
-    let alternatives ?(parse=Option.pure) f lst =
-      let open Option in
-      asum (List.map (catch f >=> parse) lst)
-
-    let alternatives_parse
-        : parse:('c -> 'a option) -> ('b -> 'c) -> 'b list -> 'a option
-      = fun ~parse f lst ->
-      let open Option in
-      asum (List.map (catch f) lst) >>= parse
-
     let read_paths paths = alternatives Prelude.readfile paths
-
-    let read_vars vars = alternatives Unix.getenv vars
-
+    let read_vars vars =
+      let each_var var =
+        var |> Unix.getenv |> Prelude.readfile
+      in
+      alternatives each_var vars 
+    let get_config_opt ~path_vars ~paths =
+      let open Option in
+      read_vars path_vars
+      <|> read_paths paths
     let get_config ~path_vars ~paths ~fallback =
       let open Option in
-      read_vars path_vars <|> read_paths paths <|> pure fallback
+      get_config_opt ~path_vars ~paths
+      <|> pure fallback
       |> get
   end
-
-  let alternatives = Simple.alternatives
-  let get_config = Simple.get_config
-
   module FCM = struct
     module type CONFPATHS = sig
       val path_vars : string list
       val paths : string list
       val fallback : string
     end
-
     let get_config (module ConfPaths : CONFPATHS) =
       Simple.get_config
         ~path_vars:ConfPaths.path_vars
@@ -37,3 +37,23 @@ module File = struct
         ~fallback:ConfPaths.fallback
   end
 end
+
+module FCM = Full.FCM
+let get_config = Full.Simple.get_config
+let get_config_opt = Full.Simple.get_config_opt
+
+module SingleOption = struct
+  module Simple = struct
+    let read_vars vars = alternatives Unix.getenv vars
+    let get_option_opt ~vars =
+      alternatives Unix.getenv vars
+    let get_option ~vars ~fallback =
+      let open Option in
+      get_option_opt ~vars
+      <|> pure fallback
+      |> get
+  end
+end
+
+let get_option = SingleOption.Simple.get_option
+let get_option_opt = SingleOption.Simple.get_option_opt
