db6ea8b79f3d0cfaab8504a3c6cbe85a3b26702a
Matt Teichman <teichman@uchicago.edu>
2023-11-13 16:31:19 -0600

    config module coming along

--- src/concrete/config.ml
+++ src/concrete/config.ml
@@ -1,53 +1,138 @@
 let alternatives lookup lst =
   let open Option in
-  asum (List.map (catch lookup) lst)
+  asum (List.map lookup lst)
 
-module Full = struct
+module Path = struct
   module Simple = struct
-    let read_paths paths =
-      alternatives Prelude.readfile paths
-    let read_vars vars =
-      let each_var var =
-        var |> Unix.getenv |> Prelude.readfile
-      in
-      alternatives each_var vars 
-    let get_config_opt ~path_vars ~paths =
+
+    let check_exists path =
+      if Sys.file_exists path
+      then Some path
+      else None
+
+    let check_varpath_exists var =
       let open Option in
-      read_vars path_vars <|> read_paths paths
-    let get_config ~path_vars ~paths ~fallback =
+      let* path = catch Sys.getenv var in
+      catch Prelude.readfile path
+
+    let check_paths paths =
+      alternatives check_exists paths
+
+    let check_vars vars =
+      alternatives check_varpath_exists vars
+
+    let get_config ~cmdline_opt ~path_vars ~paths =
       let open Option in
-      get_config_opt ~path_vars ~paths <|> pure fallback
-      |> get
+      (cmdline_opt >>= check_exists)
+      <|> check_vars path_vars
+      <|> check_paths paths
+
+    let get_config_fallback
+          ~cmdline_opt
+          ~path_vars
+          ~paths
+          ~fallback =
+      let open Option in
+      let from_system =
+        get_config ~cmdline_opt ~path_vars ~paths 
+      in default fallback from_system
+
   end
+
   module FCM = struct
+
     module type CONFPATHS = sig
+      val cmdline_opt : string option
       val path_vars : string list
       val paths : string list
+    end
+
+    module type FALLBACK = sig
+      include CONFPATHS
       val fallback : string
     end
+
     let get_config (module ConfPaths : CONFPATHS) =
       Simple.get_config
+        ~cmdline_opt:ConfPaths.cmdline_opt
         ~path_vars:ConfPaths.path_vars
         ~paths:ConfPaths.paths
-        ~fallback:ConfPaths.fallback
-  end
-end
 
-module FCM = Full.FCM
-let get_config = Full.Simple.get_config
-let get_config_opt = Full.Simple.get_config_opt
-
-module SingleOption = struct
-  module Simple = struct
-    let get_option_opt ~vars =
-      alternatives Unix.getenv vars
-    let get_option ~vars ~fallback =
+    let get_config_fallback (module Fallback : FALLBACK) =
       let open Option in
-      get_option_opt ~vars
-      <|> pure fallback
-      |> get
+      let from_system =
+        Simple.get_config
+          ~cmdline_opt:Fallback.cmdline_opt
+          ~path_vars:Fallback.path_vars
+          ~paths:Fallback.paths
+      in default Fallback.fallback from_system
+
   end
 end
 
-let get_option = SingleOption.Simple.get_option
-let get_option_opt = SingleOption.Simple.get_option_opt
+module Parse = struct
+
+  module Simple = struct
+    let alternatives lookup parse lst =
+      let open Option in
+      asum (List.map (lookup >=> parse) lst)
+  end
+
+  module FCM = struct
+
+  end
+
+end
+
+
+(* let alternatives_both lookup lst =
+ *   let open Option in
+ *   let each_path path =
+ *     let* data = catch lookup path
+ *     in pure (data, path)
+ *   in asum (List.map each_path lst)
+ * 
+ * let alternatives_data lookup lst =
+ *   Option.map fst (alternatives_both lookup lst)
+ * 
+ * let alternatives_path lookup lst =
+ *   Option.map snd (alternatives_both lookup lst)
+ * 
+ * module Full = struct
+ *   module Simple = struct
+ *     let read_paths paths =
+ *       alternatives_path Prelude.readfile paths
+ *     let read_vars vars =
+ *       let each_var var =
+ *         var |> Unix.getenv |> Prelude.readfile
+ *       in
+ *       alternatives_path each_var vars 
+ *     let get_config_opt ~path_vars ~paths =
+ *       let open Option in
+ *       read_vars path_vars <|> read_paths paths
+ *     let get_config ~path_vars ~paths ~fallback =
+ *       let open Option in
+ *       get_config_opt ~path_vars ~paths <|> pure fallback
+ *       |> get
+ *   end
+ * 
+ * end
+ * 
+ * module FCM = Full.FCM
+ * let get_config = Full.Simple.get_config
+ * let get_config_opt = Full.Simple.get_config_opt
+ * 
+ * module SingleOption = struct
+ *   module Simple = struct
+ *     let get_option_opt ~vars =
+ *       alternatives_data Unix.getenv vars
+ *     let get_option ~vars ~fallback =
+ *       let open Option in
+ *       get_option_opt ~vars
+ *       <|> pure fallback
+ *       |> get
+ *   end
+ * end
+ * 
+ * let get_option = SingleOption.Simple.get_option
+ * let get_option_opt = SingleOption.Simple.get_option_opt *)
