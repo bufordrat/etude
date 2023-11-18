ee20b443e7c2f9a28df34566e9a62da00005bf98
Matt Teichman <teichman@uchicago.edu>
2023-11-17 20:11:18 -0600

    add blurb to top of list.mli

--- src/list.mli
+++ src/list.mli
@@ -1,2 +1,9 @@
+(** Etude's list interface includes both Prelude and Stdlib's
+   respective list interfaces, which are not repeated here.  Please
+   refer to the API documentation for {{:
+   https://www2.lib.uchicago.edu/keith/software/prelude/prelude/Prelude/Lists/index.html
+   }Prelude.Lists} and {{: https://v2.ocaml.org/api/Stdlib.List.html
+   }Stdlib.List}, respectively, for more information.*)
+
 (** @inline *)
 include List_intf.AUGMENTED
