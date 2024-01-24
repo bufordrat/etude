99eb79ccae0540086e860100313f8044cd08ad89
Matt Teichman <teichman@uchicago.edu>
2024-01-24 10:03:31 -0600

    local-ify check_exists

--- src/concrete/config.ml
+++ src/concrete/config.ml
@@ -2,12 +2,12 @@ let alternatives lookup lst =
   let open Option in
   asum (List.map lookup lst)
 
-let check_exists path =
-  if Sys.file_exists path
-  then Some path
-  else None
-
 let get_config_path ?(squiggle=true) paths =
+  let check_exists path =
+    if Sys.file_exists path
+    then Some path
+    else None
+  in
   let adjusted_paths =
     if squiggle
     then List.map Prelude.File.squiggle paths
