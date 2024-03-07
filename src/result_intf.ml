b8c5b8ec2f484b921c18bcc8867f3c4c1801256f
Matt Teichman <teichman@uchicago.edu>
2024-03-07 10:55:33 -0600

    yes, this is much better, everything in Result except for monad stuff is now polymorphic

--- src/result_intf.ml
+++ src/result_intf.ml
@@ -1,33 +1,4 @@
-module type PRELUDE = sig
-
-  (* Prelude values that are polymorphic in the error type are
-     defined in MAKE below. *)
-
-  type 'a t
-  val ok : 'a -> 'a t
-  val good : 'a t -> bool
-  val bad : 'a t -> bool
-  val get_ok : 'a t -> 'a
-  val default : 'a -> 'a t -> 'a
-  val reduce : 'a t list -> 'a list
-  val always : 'a -> (unit -> 'b) -> 'b
-  val to_bool : 'a t -> bool
-  val to_option : 'a t -> 'a option
-end
-
-module type STDLIB = sig
-
-  (* Stdlib values that are polymorphic in the error type are
-     defined in MAKE below. *)
-
-  type 'a t
-  val value : 'a t -> default:'a -> 'a
-  val to_option : 'a t -> 'a option
-  val to_list : 'a t -> 'a list
-  val to_seq : 'a t -> 'a Seq.t
-end
-
-module type ETUDE = sig
+module type AUGMENTED = sig
   open Endofunctors_intf
   
   type 'a t
@@ -36,66 +7,69 @@ module type ETUDE = sig
           with type 'a t := 'a t
 end
 
-module type AUGMENTED = sig
-  type 'a t
-  include PRELUDE with type 'a t := 'a t
-  include STDLIB with type 'a t := 'a t
-  include ETUDE with type 'a t := 'a t
-end
-
 module type ERROR = sig type t end
 
 module type MAKE = 
   functor (E : ERROR) -> sig
     type 'a t = ('a, E.t) result
+    include AUGMENTED with type 'a t := ('a, E.t) result
 
-    (* Prelude values that are polymorphic in the error type go
-       here. *)
-    
-    val error : E.t -> ('a, E.t) result
-    val get_error : ('a, E.t) result -> E.t
+    (* Prelude values that are polymorphic go here. *)
+
+    val ok : 'a -> ('a, 'd) result
+    val good : ('a, 'd) result -> bool
+    val bad : ('a, 'd) result -> bool
+    val get_ok : ('a, 'd) result -> 'a
+    val default : 'a -> ('a, 'd) result -> 'a
+    val reduce : ('a, 'd) result list -> 'a list
+    val always : 'a -> (unit -> 'b) -> 'b
+    val to_bool : ('a, 'd) result -> bool
+    val to_option : ('a, 'd) result -> 'a option
+    val error : 'd -> ('a, 'd) result
+    val get_error : ('a, 'd) result -> 'd
     val on_error :
-      ('a, E.t) result ->
-      (E.t -> ('a, 'c) result) ->
+      ('a, 'd) result ->
+      ('d -> ('a, 'c) result) ->
       ('a, 'c) result
     val ( >>/ ) :
-      ('a, E.t) result ->
-      (E.t -> ('a, 'c) result) ->
+      ('a, 'd) result ->
+      ('d -> ('a, 'c) result) ->
       ('a, 'c) result
     val ( or ) :
-      ('a, E.t) result ->
+      ('a, 'd) result ->
       ('a, 'c) result ->
       ('a, 'c) result
-    val trap : (exn -> E.t) -> ('b -> 'c) -> 'b -> ('c, E.t) result
-    val trapc : E.t -> ('b -> 'c) -> 'b -> ('c, E.t) result
-    val witherr : ('a -> E.t) -> ('c, 'a) result -> ('c, E.t) result
-    val witherrc : E.t -> ('b, 'c) result -> ('b, E.t) result
-    val of_bool : ?err:E.t -> E.t -> bool -> (E.t, E.t) result
-    val of_option : E.t -> 'b option -> ('b, E.t) result
-    val some_error : ('a, E.t) result -> E.t option
+    val trap : (exn -> 'd) -> ('b -> 'c) -> 'b -> ('c, 'd) result
+    val trapc : 'd -> ('b -> 'c) -> 'b -> ('c, 'd) result
+    val witherr : ('a -> 'd) -> ('c, 'a) result -> ('c, 'd) result
+    val witherrc : 'd -> ('b, 'c) result -> ('b, 'd) result
+    val of_bool : ?err:'d -> 'd -> bool -> ('d, 'd) result
+    val of_option : 'd -> 'b option -> ('b, 'd) result
+    val some_error : ('a, 'd) result -> 'd option
 
-    (* Stdlib values that are polymorphic in the error type go
-       here *)
-    
-    val map_error : ('e -> E.t) ->
+    (* Stdlib values that are polymorphic go here *)
+
+    val value : ('a, 'd) result -> default:'a -> 'a
+    val to_option : ('a, 'd) result -> 'a option
+    val to_list : ('a, 'd) result -> 'a list
+    val to_seq : ('a, 'd) result -> 'a Seq.t
+    val map_error : ('e -> 'd) ->
                     ('a, 'e) result ->
-                    ('a, E.t) result
+                    ('a, 'd) result
     val fold : ok:('a -> 'c) ->
-               error:(E.t -> 'c) ->
-               ('a, E.t) result ->
+               error:('d -> 'c) ->
+               ('a, 'd) result ->
                'c
     val equal :
       ok:('a -> 'a -> bool) ->
-      error:(E.t -> E.t -> bool) ->
-      ('a, E.t) result ->
-      ('a, E.t) result ->
+      error:('d -> 'd -> bool) ->
+      ('a, 'd) result ->
+      ('a, 'd) result ->
       bool
     val compare :
       ok:('a -> 'a -> int) ->
-      error:(E.t -> E.t -> int) ->
-      ('a, E.t) result ->
-      ('a, E.t) result ->
+      error:('d -> 'd -> int) ->
+      ('a, 'd) result ->
+      ('a, 'd) result ->
       int
-
-    include AUGMENTED with type 'a t := ('a, E.t) result
   end
