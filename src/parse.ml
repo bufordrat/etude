bdd8d718c2d45d724663664ba4bb538e21887d7a
Matt Teichman <teichman@uchicago.edu>
2024-12-18 17:54:26 -0600

    hmm, still not quite what I'm looking for

--- src/parse.ml
+++ src/parse.ml
@@ -9,4 +9,25 @@ module Stream (Remainder : REMAINDER) = struct
       remainder : char Remainder.t }
 end
 
-module S = Stream (Seq)
+module S' = Stream (Seq)
+
+module type STREAMLINES = sig
+  type input
+  type t =
+    | Line of char list * char list option
+    | EndOfInput
+  val next : input -> t
+end
+
+(* module S : STREAMLINES with type input = string = struct
+ *   type input = string
+ *   type line =
+ *     | Line of char list * string option * string option
+ *     | EndOfInput
+ *   let next input =
+ *     let open Prelude in
+ *     match String.split ~sep:"\n" input with
+ *     | [] -> EndOfInput
+ *     | [str] -> Line (String.explode str, None, None)
+ *     | str :: strs -> Line (String.explode str, Some "\n", Some strs)
+ * end *)
