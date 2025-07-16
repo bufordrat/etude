e5d55c966ea40cc6e2b4c2e4460a2c6d5fde3a43
Matt Teichman <teichman@uchicago.edu>
2025-07-15 21:42:20 -0500

    add local opens

--- src/printer.ml
+++ src/printer.ml
@@ -1,2 +1,8 @@
-let show_to_printer to_string p x = Format.pp_print_string p (to_string x)
-let printer_to_show pp x = pp Format.str_formatter x ; Format.flush_str_formatter ()
+let show_to_printer to_string p x =
+  let open Format in
+  pp_print_string p (to_string x)
+
+let printer_to_show pp x =
+  let open Format in
+  pp str_formatter x ;
+  flush_str_formatter ()
